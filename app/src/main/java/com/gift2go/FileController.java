058720044e5d1e23731cb231ecc9d4834700e963
Cuthbert-cloud <wcuthbert17@gmail.com>
2025-11-08 14:33:00 +0000

    Gift2Go coding challenge - Controller added

---
+++ app/src/main/java/com/gift2go/FileController.java
@@ -0,0 +1,46 @@
+package com.gift2go;
+
+import com.gift2go.service.FileService;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+@Slf4j
+@RequestMapping("/v1/files")
+@RestController
+public class FileController {
+
+    private final FileService fileService;
+
+    @Autowired
+    public FileController(FileService fileService) {
+        this.fileService = fileService;
+    }
+
+    @PostMapping(value = "/process", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Resource> processFile(@RequestPart("file") MultipartFile file) throws IOException {
+        log.info("Received file: {}", file.getOriginalFilename());
+
+        File outcomeFile = fileService.processAndGenerateOutcomeFile(file);
+
+        Resource resource = new FileSystemResource(outcomeFile);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + outcomeFile.getName())
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(resource);
+    }
+}
